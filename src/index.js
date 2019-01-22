40728a73460beb99fd084904e1544879b004a65c
arturo <arturo@MacBook-Pro-2.local>
2019-01-22 11:41:59 -0500

    Line BAr Trend updated

---
+++ src/index.js
@@ -0,0 +1,156 @@
+import echarts from 'echarts';
+import color from 'color';
+import { 
+    groupAccessor,
+    firstYAxis,
+    secondYAxis,
+    firstYAxisColor,
+    secondYAxisColor,
+    firstDataAccessor, 
+    secondDataAccessor, 
+    getMetricTooltip, 
+    getYAxisData,
+    getYAxis 
+} from './utils';
+
+import './index.css';
+
+/**
+ * Global controller object is described on Zoomdata knowledge base
+ * @see https://www.zoomdata.com/developers/docs/custom-chart-api/controller/
+ */
+
+/* global controller */
+
+/**
+ * @see http://www.zoomdata.com/developers/docs/custom-chart-api/creating-chart-container/
+ */
+const chartContainer = document.createElement('div');
+chartContainer.classList.add('chart-container');
+controller.element.appendChild(chartContainer);
+
+// Dynamic the splitNumber to avoid overlap
+echarts.registerProcessor(ecModel => {
+    ecModel.findComponents({ mainType: 'yAxis' }).map(component => {
+        const defaultSplitNumber = 5;
+        const ratio = Math.floor(component.axis.grid.getRect().height / (defaultSplitNumber * component.getTextRect('0').height));
+        if (ratio < 1) component.option.axisLabel.show = false;
+        else {
+            component.option.splitNumber = ratio;
+            component.option.axisLabel.show = true;
+        }
+    });
+});
+
+const lineBar = echarts.init(chartContainer);
+
+const option = {
+    grid: {
+        left: 35,
+        top: 30,
+        right: 35,
+        bottom: 20,
+    },
+    xAxis: {
+        type: 'category',
+        axisPointer: {
+            show: true,
+            type: 'line',
+            label: {
+                show: false,
+            },
+            lineStyle: {
+                color: color(controller.variables[firstYAxisColor]).darken(.1).hex(),
+                width: 2,
+                type: 'solid',
+            }
+        },
+        splitLine: {
+            show: true,
+        },
+        axisLine: {
+            lineStyle: {
+                width: 2,
+            }
+        },
+    },
+    yAxis: [
+        getYAxis(controller.variables[firstYAxisColor]),
+        getYAxis(controller.variables[secondYAxisColor])
+    ],
+    series: [{
+            type: 'bar',
+            itemStyle: {
+                color: controller.variables[firstYAxisColor],
+            }
+        },
+        {
+            type: 'line',
+            yAxisIndex: 1,
+            itemStyle: {
+                color: controller.variables[secondYAxisColor],
+            }
+        },
+    ]
+}
+
+/**
+ * @see http://www.zoomdata.com/developers/docs/custom-chart-api/updating-queries-axis-labels/
+ */
+controller.createAxisLabel({
+    picks: groupAccessor,
+    orientation: 'horizontal',
+    position: 'bottom',
+    popoverTitle: 'Trend',
+});
+
+controller.createAxisLabel({
+    picks: firstYAxis,
+    orientation: 'vertical',
+    position: 'left',
+});
+
+controller.createAxisLabel({
+    picks: secondYAxis,
+    orientation: 'vertical',
+    position: 'right',
+});
+
+controller.resize = () => lineBar.resize();
+
+/**
+ * @see http://www.zoomdata.com/developers/docs/custom-chart-api/receiving-chart-data/
+ */
+controller.update = data => {
+    option.xAxis.data = data.map(datum => controller.dataAccessors[groupAccessor].format(_.first(datum.group)));
+    _.first(option.series).data = getYAxisData(data, firstDataAccessor.getMetric());
+    _.last(option.series).data = getYAxisData(data, secondDataAccessor.getMetric());
+    lineBar.setOption(option);
+};
+
+// Tooltip
+lineBar.on('mousemove', params => {
+    if (params && params.data && _.isObject(params.data.datum)) {
+        controller.tooltip.show({
+            x: params.event.event.clientX,
+            y: params.event.event.clientY,
+            content: () => {
+                return getMetricTooltip(params);
+            }
+        });
+    }
+});
+
+lineBar.on('mouseout', () => {
+    controller.tooltip.hide();
+});
+
+// Menu bar
+lineBar.on('click', params => {
+    controller.tooltip.hide();
+    controller.menu.show({
+        x: params.event.event.clientX,
+        y: params.event.event.clientY,
+        data: () => params.data.datum,
+    });
+});
---
+++ src/utils.js
@@ -0,0 +1,92 @@
+import visualization from '../visualization.json';
+
+export const groupAccessor = visualization.variables[4].name
+export const firstYAxis = visualization.variables[0].name;
+export const secondYAxis = visualization.variables[2].name;
+export const firstYAxisColor = visualization.variables[1].name;
+export const secondYAxisColor = visualization.variables[3].name;
+export const firstDataAccessor = controller.dataAccessors[firstYAxis];
+export const secondDataAccessor= controller.dataAccessors[secondYAxis];
+
+const getTableRow = (label, value, color='') => `<div class="zd_tooltip_info_table_row"><div class="zd_tooltip_info_table_row_label">${label}</div><div class="zd_tooltip_info_table_row_value">${color} ${value}</div></div>`;
+
+/**
+ * Get Trend Attribute label
+ */
+const getGeneralLabel = () => {
+    const accessor = controller.dataAccessors[groupAccessor];
+    const accessorGroup = accessor.getGroup();
+    return `${accessorGroup.label} ${accessor._timeZoneId} (${accessorGroup.func})`;
+}
+
+/**
+ * Get tooltip for Y Axis
+ * @param {Object} params 
+ * @param {Object} compareName 
+ */
+const getYAxisTooltip = (params, accessor, compareName=null) => {
+    const metric = accessor.getMetric();
+    if (metric.name !== 'count' && metric.name !== compareName) {
+        return `<div class="zd_tooltip_info_table_row">${getTableRow(`${metric.label} (${metric.func})`, `${accessor.formatted(params.data.datum)}`)}</div>`
+    }
+    return '';
+}
+
+const getMetric = params => {
+    if (_.has(params, 'data.datum.current.count')) {
+        // Color always put before Volumen value
+        const color = `<div class="color_icon active" style="background-color: ${params.color};"></div>`;
+        const firstAccessorRow = getYAxisTooltip(params, firstDataAccessor);
+        const secondAccessorRow = getYAxisTooltip(params, secondDataAccessor, firstDataAccessor.getMetric().name);
+        return `<div class="zd_tooltip_info_table_row">${getTableRow('Volume', `${params.data.datum.current.count}`, color)}</div>${firstAccessorRow}${secondAccessorRow}`;
+    }
+    return '';
+}
+
+const getNormalMetricData = data => data.map(datum => ({ value: datum.current.count, datum }));
+
+/**
+ * Format number to k, M, G (thousand, Million)
+ * @param {Number} number 
+ * @param {Number} digits 
+ */
+const SIFormat = (number, digits=0) => {
+    const codeTable = ['p', 'n', 'u', 'm', '', 'k', 'M', 'G', 'T'];
+    const [exponentialNumber, exponential] = number.toExponential(digits).split('e+');
+    const index = Math.floor(_.parseInt(exponential) / 3);
+    return exponentialNumber * Math.pow(10, _.parseInt(exponential) - index * 3) + codeTable[index + 4];
+}
+
+export const getMetricTooltip = params => {
+    if (params && _.has(params, 'name') && _.has(params, 'color') && _.has(params, 'data.value')) {
+        const label = getGeneralLabel();
+        const metric = getMetric(params);
+        return `<div class="zd_tooltip_info_group customized"><div class="zd_tooltip_info_table"><div class="zd_tooltip_info_table_row">${getTableRow(label, params.name)}</div>${metric}</div></div>`;
+    }
+    return '';
+};
+
+export const getYAxisData = (data, metric) => {
+    if (metric.name != visualization.variables[0].defaultValue[0].name) {
+        return data.map(datum => ({ value: datum.current.metrics[metric.name][metric.func], datum }));
+    }
+    return getNormalMetricData(data);
+}
+
+export const getYAxis = color => ({
+        type: 'value',
+        name: 'Bar',
+        axisLine: {
+            lineStyle: {
+                color: color,
+                width: 2,
+            }
+        },
+        axisLabel: {
+            color: '#000',
+            formatter: value => SIFormat(value, 2),
+        },
+        axisTick: {
+            show: false,
+        }
+});
